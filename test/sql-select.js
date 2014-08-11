c402345a26b547417779a2eb61a78566233fcdf2
Jakub Stefanski <j.stefanski@makebytes.com>
2014-08-11 23:47:32 +0200

    Basic SELECT with WHERE clause

--- test/sql-select.js
+++ test/sql-select.js
@@ -4,8 +4,40 @@ var sqlBuilder = require('../lib/sql-builder');
 describe('SELECT queries', function () {
   describe('Basic example', function () {
     it('should generate basic SELECT query', function () {
-      var sql = sqlBuilder.browse({ tableName: 'users' });
-      sql.should.be.exactly('SELECT * FROM users;');
+      var result = sqlBuilder.browse({ tableName: 'users' });
+      result.sql.should.be.exactly('SELECT * FROM users;');
+    });
+
+    it('should generate string filtering', function () {
+      var result = sqlBuilder.browse({ 
+        tableName: 'users',
+        where: "name LIKE '%algotronic%'" 
+      });
+      result.sql.should.be.exactly("SELECT * FROM users WHERE name LIKE '%algotronic%';");
+    });
+
+    it('should generate parametrized string filtering', function () {
+      var result = sqlBuilder.browse({ 
+        tableName: 'accounts',
+        where: ["user_id = $1", 5] 
+      });
+      result.sql.should.be.exactly("SELECT * FROM accounts WHERE user_id = $1;");
+    });
+
+    it('should generate function filtering', function () {
+      var result = sqlBuilder.browse({ 
+        tableName: 'users',
+        where: function () { return "name LIKE '%algotronic%'"; }
+      });
+      result.sql.should.be.exactly("SELECT * FROM users WHERE name LIKE '%algotronic%';");
+    });
+
+    it('should generate parametrized function filtering', function () {
+      var result = sqlBuilder.browse({ 
+        tableName: 'accounts',
+        where: function (req) { return ["user_id = $1", 5]; }
+      });
+      result.sql.should.be.exactly("SELECT * FROM accounts WHERE user_id = $1;");
     });
   });
 });
